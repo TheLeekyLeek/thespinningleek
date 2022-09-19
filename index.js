506f9285ac09038577fcd378e265aabf40702240
Taxen99 <71523776+Taxen99@users.noreply.github.com>
2022-09-19 10:25:36 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+let d10, d50, d100, d200, d500, d1000;
+
+d10   = document.getElementById("d10");
+d50   = document.getElementById("d50");
+d100  = document.getElementById("d100");
+d200  = document.getElementById("d200");
+d500  = document.getElementById("d500");
+d1000 = document.getElementById("d1000");
+
+d10.onclick   = _ => donate(10);
+d50.onclick   = _ => donate(50);
+d100.onclick  = _ => donate(100);
+d200.onclick  = _ => donate(200);
+d500.onclick  = _ => donate(500);
+d1000.onclick = _ => donate(1000);
+
+const creditcardRegex = /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/;
+
+function donate(amount) {
+    let card;
+    let text = "Please enter your credit card number (0000-0000-0000-0000)";
+    while(!creditcardRegex.test(card = prompt(text))) {
+        text = "Please enter a valid credit card number.";
+    }
+    alert(`Successfully donated ${amount} to the Ann Party`);
+}
